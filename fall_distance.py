44c2f2beeba37ede603df8f53337601de197817a
acctwdxab <71532324+acctwdxab@users.noreply.github.com>
2020-10-19 16:53:12 -0700

    Add files via upload

---
+++ fall_distance.py
@@ -0,0 +1,8 @@
+# Dan Wu
+# 10/19/2020
+# Create a function and calculate the distance an object falls due to gravity in a specific time period
+
+def fall_distance(sec):
+    """ The distance function returns the distance in meters based on the time object has fallen due to gravity."""
+    return float(0.5*9.8*sec**2);
+
